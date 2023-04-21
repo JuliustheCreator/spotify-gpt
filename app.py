d97f77a9feb5c651e549e830cb12fca354a4b49b
JuliustheCreator <Jrbroomfield1@gmail.com>
2023-04-21 02:52:40 -0400

    fixed backend; added 'add songs' button

--- app.py
+++ app.py
@@ -27,30 +27,47 @@ def get_response(prompt):
 st.title("Spotify Music Recommendation")
 st.write("Music recommendation system built with Generative Pre-trained Transformer")
 
-st.write("Name as many of your favorite songs as you'd like! (of the same genre for best results)")
+# Backend
+import streamlit as st
+import time
+
+def get_response(text):
+    # Replace this function with your GPT-3 implementation
+    return "These are the recommended songs based on your input."
+
+
+# Frontend
+st.title("Spotify Music Recommendation")
+st.write("Music recommendation system built with Generative Pre-trained Transformer")
 
 # Backend
-
-button = st.button('Finish')
 songs = []
-TEXT = f'Name 10 obscure artists I am sure to enjoy, as well as a recommended song of theirs if I enjoy listening to: {songs}'
+song_list_placeholder = st.empty()
 
 # Adding User's Songs to List
-user_input = st.text_input("Input Song and Artist: ")
-while (not button) and len(songs):
+user_input = st.text_input("Input Song and Artist:")
+
+add_song_button = st.button("Add Song")
+submit_button = st.button("Submit Songs")
+
+if add_song_button and user_input:
     songs.append(user_input)
-    user_input = st.text_input("Input Song and Artist: ")
+    song_list_placeholder.write(f"Current song list: {', '.join(songs)}")
+    user_input = ""  # Clear the input box
 
 # Fetching Response
-response_text = get_response(TEXT)
+if submit_button and len(songs) > 0:
+    TEXT = f'Name 10 obscure artists I am sure to enjoy, as well as a recommended song of theirs if I enjoy listening to: {songs}'
+    response_text = get_response(TEXT)
 
-# Progress Bar
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-    bar.progress(i + 1)
-    time.sleep(0.1)
+    # Progress Bar
+    st.write("Fetching recommendations...")
+    bar = st.progress(0)
+    for i in range(100):
+        bar.progress(i + 1)
+        time.sleep(0.01)
 
-# Print Response
-st.write(response_text)
-
+    # Print Response
+    st.write(response_text)
+elif submit_button:
+    st.warning("Please add at least one song before submitting.")
