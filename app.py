99df9c273f9a904a241ff6a1416943ef1d986879
JuliustheCreator <Jrbroomfield1@gmail.com>
2023-04-21 03:14:45 -0400

    added spotify api

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import openai
 import streamlit as st
 import time
+from spotipy.oauth2 import SpotifyOAuth
+import urllib.parse
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -10,6 +12,36 @@ load_dotenv()
 # Set the OpenAI API key
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+# Setting up Spotify Auth
+client_id = 'your_client_id'
+client_secret = 'your_client_secret'
+redirect_uri = 'https://your_streamlit_app_url/callback'
+scope = 'playlist-modify-public'
+
+auth_manager = SpotifyOAuth(client_id=client_id,
+                            client_secret=client_secret,
+                            redirect_uri=redirect_uri,
+                            scope=scope,
+                            show_dialog=True)
+
+# Check if an authorization code is in the URL query parameters
+query_params = st.experimental_get_query_params()
+auth_code = query_params.get('code', [None])[0]
+
+if auth_code:
+    # Get access token using the authorization code
+    auth_manager.get_access_token(auth_code)
+    
+    if auth_manager.get_cached_token():
+        # Proceed with the authenticated actions (e.g., creating playlists)
+        st.write("You are now authenticated.")
+    else:
+        st.warning("Failed to authenticate.")
+else:
+    # Redirect the user to the Spotify authorization URL
+    auth_url = auth_manager.get_authorize_url()
+    st.markdown(f"[Click here to authenticate with Spotify.]({auth_url})")
+    
 # Send a message request and get the response
 def get_response(prompt):
     response = openai.Completion.create(
@@ -23,7 +55,7 @@ def get_response(prompt):
     )
     return response.choices[0].text.strip()
 
-# Frontend
+# C
 st.title("Spotify Music Recommendation")
 st.write("Music recommendation system built with Generative Pre-trained Transformer")
 
@@ -32,15 +64,16 @@ songs = []
 song_list_placeholder = st.empty()
 
 # Adding User's Songs to List
-user_input = st.text_input("Input Song and Artist:")
+user_input = st.text_input('', placeholder = "Input Song and Artist")
 
-add_song_button = st.button("Add Song")
-submit_button = st.button("Submit Songs")
+add_song_button = st.button("Add Song.")
+st.empty()
+submit_button = st.button("Submit Songs.")
 
 if add_song_button and user_input:
     songs.append(user_input)
-    song_list_placeholder.write(f"Current song list: {', '.join(songs)}")
-    user_input = ""  # Clear the input box
+    song_list_placeholder.write(f"Songs & Artist: {', '.join(songs)}")
+    user_input = "" 
 
 # Fetching Response
 if submit_button and len(songs) > 0:
