5a6deb49fc3b200af2a9365cb71486033e5463a7
JuliustheCreator <Jrbroomfield1@gmail.com>
2024-03-02 03:48:22 -0500

    added frontend-backend functionality

--- src/app.py
+++ src/app.py
@@ -1,48 +1,39 @@
-from flask import Flask, request, render_template, session
-from flask_session import Session  # You may need to install Flask-Session
+from flask import Flask, request, render_template
 import os
 from dotenv import load_dotenv
 import openai
+import json
 
 
 
 load_dotenv()
 openai.api_key = os.environ.get("OPENAI_API_KEY")
-
 app = Flask(__name__)
-app.secret_key = 'ANGP0618'  
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if 'songs' not in session:
-        session['songs'] = []
-    
     recommendations = ""
+    x = 10
     if request.method == "POST":
-        song_action = request.form.get("action")
-        song = request.form.get("song")
-        
-        if song_action == "Add Song" and song:
-            session['songs'].append(song)
-            session.modified = True 
-        elif song_action == "Submit Songs" and session['songs']:
-            prompt = f'Name 10 obscure artists I am sure to enjoy, as well as a recommended song of theirs if I enjoy listening to: {session["songs"]}'
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=prompt,
-                temperature=0.7,
-                max_tokens=256,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0
+        songs_json = request.form.get("songs")
+        if songs_json:
+            songs = json.loads(songs_json)
+            prompt = f'''
+            List {x} lesser-known artists for someone with the following song preferences. Prioritize uniqueness and obscurity in your recommendations:\n
+            {songs}
+            '''
+
+            completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a music recommender system, with a deep understanding of modern music and pop culture. You will recommend artists to the users, you should say absolutely nothing else but each artist's names seperated by new lines."},
+                {"role": "user", "content": prompt}
+            ]
             )
-            recommendations = response.choices[0].text.strip()
-            session['songs'].clear() 
-            
-    return render_template("index.html", songs=session.get('songs', []), recommendations=recommendations)
+            recommendations = (completion.choices[0].message)['content'].split('\n')
+
+    return render_template("index.html", recommendations=recommendations)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
